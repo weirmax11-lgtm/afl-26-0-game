a3c237d592f11e492dd113ce2d0b7e5fd24e2482
weirmax11-lgtm <weirmax11@gmail.com>
2026-06-25 14:16:42 -0700

    Update Wheel.js

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,3 +1,19 @@
-export default function Wheel() {
-  return <div>Wheel coming soon</div>;
+const teams = [
+  "Adelaide", "Brisbane Lions", "Brisbane Bears", "Carlton",
+  "Collingwood", "Essendon", "Fitzroy", "Fremantle",
+  "Geelong", "GWS", "Hawthorn", "Melbourne",
+  "North Melbourne", "Port Adelaide", "Richmond",
+  "St Kilda", "Sydney", "West Coast", "Western Bulldogs"
+];
+
+const eras = [
+  "1960s", "1970s", "1980s", "1990s",
+  "2000s", "2010s", "2020s"
+];
+
+export function spinWheel() {
+  const team = teams[Math.floor(Math.random() * teams.length)];
+  const era = eras[Math.floor(Math.random() * eras.length)];
+
+  return { team, era };
 }
