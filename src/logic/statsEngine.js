3ef9145aec208802016a762bf870bec7c13933ad
weirmax11-lgtm <weirmax11@gmail.com>
2026-06-25 14:17:05 -0700

    Update statsEngine.js

--- src/logic/statsEngine.js
+++ src/logic/statsEngine.js
@@ -1,3 +1,13 @@
-export function test() {
-  return "logic works";
+export function calculatePlayerAverages(player) {
+  const games = player.GM || 1;
+
+  return {
+    marks: player.MK / games,
+    disposals: player.DI / games,
+    goals: player.GL / games,
+    behinds: player.BH / games,
+    hitouts: player.HO / games,
+    tackles: player.TK / games,
+    brownlow: player.BR / games
+  };
 }
